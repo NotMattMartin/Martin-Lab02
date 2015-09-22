549e263762506c45767b146986136185f4b8cddc
5jw <5jw@kek.club>
2015-09-22 11:54:46 -0400

    Initial

---
+++ Lab02.java
@@ -0,0 +1,24 @@
+
+/**
+Matt Martin
+Copy and Paste v1
+ */
+public class Lab02
+{
+    public static void main(String args[])
+    {
+        System.out.println("Lab 02C, 100 Point Verson");
+        System.out.println();
+        System.out.println("First Java Program");
+        System.out.println();
+        System.out.println("Measure today's knowledge by yesterday's confusion");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("#  #####  #####      #####  #####  #  #   #  #####  #####");
+        System.out.println("#  #   #  #   #      #   #  #   #  #  ##  #    #    #");
+        System.out.println("#  #   #  #   #      #####  #   #  #  # # #    #    #####");
+        System.out.println("#  #   #  #   #      #      #   #  #  #  ##    #        #");
+        System.out.println("#  #####  #####      #      #####  #  #   #    #    #####");
+    }
+}
